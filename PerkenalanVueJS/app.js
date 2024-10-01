f25f7557d661dd97d785f8083ab40cbcccb5e0e4
Eri Maulana <maulanabouble11@gmail.com>
2024-10-01 10:18:55 +0700

    1.3 Cara Binding Data ke dalam Attribute HTML

--- PerkenalanVueJS/app.js
+++ PerkenalanVueJS/app.js
@@ -2,7 +2,8 @@ var product = {
     "name": "Bamboo Thermal Ski Coat",
     "description": "You'll be the most environmentally conscious skier on the slopes - and the most stylish - wearing our fitted bamboo thermal ski coat, made from organic bamboo with recycled plactic down filling.",
     "price": "99",
-    "image": "https://hplussport.com/wp-content/uploads/2016/12/In-the-Kitchen-with-H-book-cover.png"
+    "image": "https://hplussport.com/wp-content/uploads/2016/12/In-the-Kitchen-with-H-book-cover.png",
+    "toko": "Eri maulana"
 }
 
 var app = new Vue({
