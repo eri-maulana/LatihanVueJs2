eb5dcdfb7e16b67d33d9da343c52cfbd1f762e8f
Eri Maulana <maulanabouble11@gmail.com>
2024-10-01 09:29:36 +0700

    1.2 Mengenal Reactive Data pada VueJS

--- PerkenalanVueJS/app.js
+++ PerkenalanVueJS/app.js
@@ -1,6 +1,11 @@
+var product = {
+    "name": "Bamboo Thermal Ski Coat",
+    "description": "You'll be the most environmentally conscious skier on the slopes - and the most stylish - wearing our fitted bamboo thermal ski coat, made from organic bamboo with recycled plactic down filling.",
+    "price": "99",
+    "image": "https://hplussport.com/wp-content/uploads/2016/12/In-the-Kitchen-with-H-book-cover.png"
+}
+
 var app = new Vue({
     el: '#app',
-    data: {
-        name: 'Vue JS'
-    }
+    data: product
 })
