a45c9bd0a1ce5ef83d890e0cf4f3f9c4ce6181b0
Eri Maulana <maulanabouble11@gmail.com>
2024-10-02 09:16:39 +0700

    1.8 Mengenal Event dan Method di dalam VueJS

--- PerkenalanVueJS/app.js
+++ PerkenalanVueJS/app.js
@@ -5,6 +5,7 @@ var app = new Vue({
   data: {
     maximum: 50,
     products: null,
+    cart: [],
   },
   mounted: function() {
     fetch('https://hplussport.com/api/products/order/price')
@@ -12,5 +13,10 @@ var app = new Vue({
     .then(data => {
         this.products = data;
     })
-  }
+  },
+  methods: {
+    addItem: function (product){
+        this.cart.push(product)
+    }
+  },
 });
