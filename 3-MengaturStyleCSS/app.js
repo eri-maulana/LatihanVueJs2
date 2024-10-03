c1220a26c37ec36cc4c126c21be7af2dae400c95
Eri Maulana <maulanabouble11@gmail.com>
2024-10-03 09:27:11 +0700

    3.1 Binding Attribute dengan Array dan Object

---
+++ 3-MengaturStyleCSS/app.js
@@ -0,0 +1,27 @@
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+      maximum: 50,
+      products: null,
+      cart: [],
+      style: {
+        label: ['font-weight-bold', 'mr-2'],
+        style: {'width': '60px', 'text-align': 'center'}
+      }
+    },
+    mounted: function() {
+      fetch('https://hplussport.com/api/products/order/price')
+      .then(response => response.json())
+      .then(data => {
+          this.products = data;
+      })
+    },
+    methods: {
+      addItem: function (product){
+          this.cart.push(product)
+      }
+    },
+  });
+  
