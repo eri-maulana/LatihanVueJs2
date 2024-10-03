2f04e95c85677273963e0f6b483d71ad6feaa379
Eri Maulana <maulanabouble11@gmail.com>
2024-10-03 11:22:56 +0700

    3.7 Mengatur Style dengan JavaScript

--- 3-MengaturStyleCSS/app.js
+++ 3-MengaturStyleCSS/app.js
@@ -27,7 +27,22 @@ var app = new Vue({
     methods: {
       addItem: function (product){
           this.cart.push(product)
-      }
+      },
+      before: function (el){
+        el.className = 'd-none';
+      },
+      enter: function (el){
+        var delay = el.dataset.index * 100;
+        setTimeout(function(){
+          el.className = 'row d-flex mb-3 align-items-center animated fadeInRight';
+        }, delay);
+      },
+      leave: function (el){
+        var delay = el.dataset.index * 100;
+        setTimeout(function(){
+          el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight';
+        }, delay);
+      },
     },
   });
   
